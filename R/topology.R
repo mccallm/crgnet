ae1b9fbbfd4ab9e8c229c9187e27c93927fe50d3
mccallm <mccallm@gmail.com>
2024-03-07 10:13:07 -0500

    Add weighting option to topology function

--- R/topology.R
+++ R/topology.R
@@ -1,13 +1,21 @@
-topology <- function(fits){
+topology <- function(fits, weighted=FALSE){
   ngene <- nrow(fits[[1]]$parents)
   nexp <- length(fits[[1]]$trajectories)
   grph <- matrix(0, nrow=ngene, ncol=nexp)
   for(k in 1:length(fits)){
     gtmp <- t(fits[[k]]$parents + 1)
     for(j in 1:ncol(gtmp)){
-      grph[j, gtmp[,j]] <- grph[j, gtmp[,j]] + 1
+      if(weighted){
+        grph[j, gtmp[,j]] <- grph[j, gtmp[,j]] + 1/fits[[k]]$unnormalized_score
+      } else{
+        grph[j, gtmp[,j]] <- grph[j, gtmp[,j]] + 1
+      }
     }
   }
-  cgrph = grph / length(fits)
+  if(weighted){
+    cgrph = grph / sum(sapply(fits, function(x) x$unnormalized_score))
+  } else{
+    cgrph = grph / length(fits)
+  }
   return(cgrph)
 }
